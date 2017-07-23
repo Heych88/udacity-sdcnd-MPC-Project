585a6b686fd173bf7cca2144e8610711d1cf01ed
Haidyn <haidyn1988@gmail.com>
2017-07-23 23:35:31 +1000

    adjust spaces

--- src/MPC.h
+++ src/MPC.h
@@ -7,14 +7,14 @@
 using namespace std;
 
 class MPC {
- public:
-  MPC();
+  public:
+    MPC();
 
-  virtual ~MPC();
+    virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
