4085cff1d423552b04ae388080421110ba5d0c48
haidyn <haidyn1988@gmail.com>
2017-07-24 10:40:32 +1000

    display controlled path

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,7 +6,7 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 20;
+size_t N = 12;
 double dt = 0.1;
 double ref_v = 40;
 
@@ -49,9 +49,9 @@ class FG_eval {
     fg[0] = 0;
     
     for(int i=0; i < N; i++) {
-      fg[0] += 10 * CppAD::pow(vars[cte_start + i], 2);
-      fg[0] += 10 * CppAD::pow(vars[epsi_start + i], 2);
-      fg[0] += 100 * CppAD::pow(vars[v_start + i] - ref_v, 2);
+      fg[0] += CppAD::pow(vars[cte_start + i], 2);
+      fg[0] += CppAD::pow(vars[epsi_start + i], 2);
+      fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
     }
     
     for(int i=0; i < N-1; i++) {
@@ -60,8 +60,8 @@ class FG_eval {
     }
     
     for(int i=0; i < N-2; i++) {
-      fg[0] += CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
-      fg[0] += CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+      fg[0] += 10 * CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+      fg[0] += 20 * CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
     }
     
     // Initial constraints
@@ -77,7 +77,7 @@ class FG_eval {
     fg[1 + epsi_start] = vars[epsi_start];
 
     // The rest of the constraints
-    for (int t = 0; t < N - 2; t++) {
+    for (int t = 0; t < N - 1; t++) {
       AD<double> x1 = vars[x_start + t + 1];
       AD<double> y1 = vars[y_start + t + 1];
       AD<double> psi1 = vars[psi_start + t + 1];
@@ -231,8 +231,12 @@ std::tuple<vector<double>, vector<double>, vector<double>> MPC::Solve(Eigen::Vec
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
   vector<double> control = {solution.x[delta_start], solution.x[a_start]};
-  vector<double> path_x(solution.x[x_start], solution.x[x_start + N]);
-  vector<double> path_y(solution.x[y_start], solution.x[y_start + N]);
+  vector<double> path_x;
+  vector<double> path_y;
+  for (int i=0; i < N; i++) {
+    path_x.push_back(solution.x[x_start + i]);
+    path_y.push_back(solution.x[y_start + i]);
+  }
 
   return std::make_tuple(control, path_x, path_y);
 }
